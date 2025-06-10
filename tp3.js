8e7b926609bfd7a6be6a23fc4590a0586a82564f
DamarisMusic <49333734@ivt.edu.ar>
2025-06-09 21:09:35 -0300

    cambios

---
+++ tp3.js
@@ -0,0 +1,37 @@
+//Ejercicio 1
+let animales = ['perro', "gato", "conejo", "loro"]
+
+for(let i = 0; i < animales.length; i++){
+    console.log(animales[i]);
+}
+
+//Ejercicio 2
+for(let i = 0; i < animales.length; i++){
+    if(i % 2 === 0) {
+        console.log(animales[i]);
+    }
+}
+
+//Ejercicio 3
+let numeros = [4, 7, 2, 9, 5];
+let suma = 0
+for(let i = 0; i < numeros.length; i++){
+    suma += numeros[i];
+    console.log('La suma es'+  suma)
+}
+
+//Ejercicio 4
+let datos = [3, 6, 1, 8];
+
+let multiplicacion = []
+for(let i = 0; i < datos.length; i++){
+    multiplicacion.push(datos[i] * 2)
+}
+console.log(multiplicacion)
+
+//Ejercicio 5 ?
+let alumnos = ["Sofía", "Carlos", "Bruno", "Lucía", "Carla"];
+let nombres = prompt('Ingresa cualquier nombre')
+
+//Ejercicio 6
+let valores = [5, 0, 8, 0, 2];
