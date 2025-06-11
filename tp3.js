8289ed5426e63b4b87c9f800c72a36b9f6ce57f5
alumno <alumno@Instituto.local>
2025-06-11 08:18:45 -0300

    cambios clase

--- tp3.js
+++ tp3.js
@@ -20,6 +20,7 @@ for(let i = 0; i < numeros.length; i++){
     console.log('La suma es'+  suma)
 }
 
+
 //Ejercicio 4
 let datos = [3, 6, 1, 8];
 
@@ -32,6 +33,15 @@ console.log(multiplicacion)
 //Ejercicio 5 ?
 let alumnos = ["Sofía", "Carlos", "Bruno", "Lucía", "Carla"];
 let nombres = prompt('Ingresa cualquier nombre')
+while(nombres = alumnos){
+    console.log()
+}
 
 //Ejercicio 6
 let valores = [5, 0, 8, 0, 2];
+console.log(valores)
+let pos = 1
+let pos2 = 3
+let valoresCambiados = valores.splice(pos,pos2)
+console.log(valoresCambiados)
+
