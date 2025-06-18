5204e393c25a85cf72bc7c237a6ef84548a8aae9
alumno <alumno@Instituto.local>
2025-06-18 08:20:15 -0300

    cambios

---
+++ practica.js
@@ -0,0 +1,17 @@
+let numeros = [6,7,8,9,4,2]
+let cantidadNumeros = numeros.length
+
+.for (let i = 0; i < cantidadNumeros; i++) {
+    numeros[i] = numeros[i]*2 //si queremos que se multiplique
+    numeros[i] = numeros[i]+1 //si queremos que se sume
+    
+}
+console.log(numeros)
+
+//reemplazar 0 por 100
+let nums = [20,0,9,0]
+for (let i = 0; i < array.length; i++) {
+   if(numeros[i] == 0){
+    numeros[i] = 100
+   }
+}
