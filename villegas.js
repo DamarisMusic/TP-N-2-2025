c9dbf4f73ab86ff9fa6f3a9774f147c05bea1db9
alumno <alumno@Instituto.local>
2025-08-06 08:30:27 -0300

    examen06

---
+++ villegas.js
@@ -0,0 +1,34 @@
+//1
+let temperaturas = [30, 20, 28, 16, 13, 25, 23]
+let sumaTemp = 0
+for (let indice = 0; indice < temperaturas.length; indice++) {
+    sumaTemp += temperaturas / 7
+    console.log(temperaturas)
+}
+
+//2
+let numeros = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+let indice = 0
+while(indice < numeros.length){
+    if(numeros % 1 === 0){
+        i++
+    }
+    console.log(numeros[indice])
+}
+
+//3
+let frutas = ['banana', 'manzana', 'pera', 'durazno', 'naranja', 'uva']
+for (let indice = 0; indice < frutas.length; indice++) {
+    if(indice = 3){
+        alert('la fruta se encuentra en la posición 3')
+    }
+    
+}
+
+//4
+let precios = [100, 250, 80, 300, 150]
+let multiplicacio = []
+for (let indice = 0; indice < precios.length; indice++) {
+    multiplicacion.push(precios[i] * 1.21)
+    console.log(multiplicacion)
+}
